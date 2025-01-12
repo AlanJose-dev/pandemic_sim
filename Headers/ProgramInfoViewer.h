ed7cdb490dd5a2a88bc6f21fddadfbad44d6d72a
AlanJose-dev <alanjsdelima@gmail.com>
2025-01-12 10:42:50 -0300

    Updating help

--- Headers/ProgramInfoViewer.h
+++ Headers/ProgramInfoViewer.h
@@ -48,9 +48,9 @@ void printHelp()
 {
     printASCIIArt();
     cout << "-------------------------------------------------------------------------------------------" << endl;
-    cout << "Usage: simulator [-v] [-h] [-r <value>] [-p <value>]" << endl;
-    cout << "                 [-g <value>] [-s] [-t <value>]" << endl;
-    cout << "                 [-c <value>] [-i]" << endl;
+    cout << "Usage: simulator [-v | --version] [-h | --help] [-r | --runs <value>] [-p | --population <value>]" << endl;
+    cout << "                 [-g | --generations <value>] [-s | --social-distance-effect] [-t | --threads <value>]" << endl;
+    cout << "                 [-c | --contagion-factor <value>] [-o | --output-state <value>] [-i | --image]" << endl;
     cout << "\n" << endl;
     cout << "Multithreading is available : " << boolToString(MultithreadingController::currentProcessorSupportsMultithreading()) << "." << endl;
     cout << "CPU Threads available       : " << MultithreadingController::getCurrentProcessorAvailableThreads() << "." << endl;
@@ -62,17 +62,17 @@ void printHelp()
     cout << "Dead                        : " << "3" << endl;
     cout << "Immune                      : " << "4" << endl;
     cout << "\n" << endl;
-    cout << "Parameter descriptions      :" << endl;
-    cout << "-v (version)                :       Show the program version." << endl;
-    cout << "-h (help)                   :       Show this message." << endl;
-    cout << "-r (runs)                   :       Define how many times the model will be executed, determining the number of results (integer)." << endl;
-    cout << "-p (population)             :       Define the population matrix side length. Use the square root, e.g., 100 corresponds to 10,000 (integer)." << endl;
-    cout << "-g (generations)            :       Specify the number of generations in weeks (integer)." << endl;
-    cout << "-s (social distance effect) :       Run the simulations with the social distancing/lockdown effect applied, reducing the disease contagion factor." << endl;
-    cout << "-t (threads)                :       Run the simulations with a multi-threaded profile. Specifies the number of threads the program may use. The maximum value is the number of threads available on the current processor (integer)." << endl;
-    cout << "-c (contagion factor)       :       Defines the disease contagion factor, minimum 0.1, maximum 1 (double)." << endl;
-    cout << "-o (state to output)        :       Defines the state for which you want to obtain the number of affected individuals (integer)." << endl;
-    cout << "-i (image on finish)        :       Generate a visual disease spread example as a .png image." << endl;
+    cout << "Parameter descriptions        :" << endl;
+    cout << "-v | --version                :       Show the program version." << endl;
+    cout << "-h | --help                   :       Show this message." << endl;
+    cout << "-r | --runs                   :       Define how many times the model will be executed, determining the number of results (integer)." << endl;
+    cout << "-p | --population             :       Define the population matrix side length. Use the square root, e.g., 100 corresponds to 10,000 (integer)." << endl;
+    cout << "-g | --generations            :       Specify the number of generations in weeks (integer)." << endl;
+    cout << "-s | --social-distance-effect :       Run the simulations with the social distancing/lockdown effect applied, reducing the disease contagion factor." << endl;
+    cout << "-t | --threads                :       Run the simulations with a multi-threaded profile. Specifies the number of threads the program may use. The maximum value is the number of threads available on the current processor (integer)." << endl;
+    cout << "-c | --contagion-factor       :       Defines the disease contagion factor, minimum 0.1, maximum 1 (double)." << endl;
+    cout << "-o | --output-state           :       Defines the state for which you want to obtain the number of affected individuals (integer)." << endl;
+    cout << "-i | --image                  :       Generate a visual disease spread example as a .png image." << endl;
     cout << "---------------------------------------------------------------------------------------------" << endl;
     cout << "Default params: r(100), p(100), p(10), c(0.5), o(3), s(false), t(1), i(false)" << endl;
 }
